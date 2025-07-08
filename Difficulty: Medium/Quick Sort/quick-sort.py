b942759e639a131a37b24bc3295f925c206873b4
Anusha Syeda <anusha.bcss19@iba-suk.edu.pk>
2025-07-08 15:31:55 +0500

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Quick Sort/quick-sort.py
@@ -0,0 +1,26 @@
+class Solution:
+    #Function to sort a list using quick sort algorithm.
+    def quickSort(self,arr,low,high):
+        # code here
+        if low <= high:
+            partitionIndex = self.partition(arr, low, high);
+            self.quickSort(arr,low,partitionIndex-1);
+            self.quickSort(arr,partitionIndex+1, high);
+            
+            
+    
+    def partition(self,arr,low,high):
+        # code here
+        pivot = arr[low];
+        i = low;
+        j = high;
+        while i < j:
+            while(arr[i] <= pivot and i <= high-1):
+                i += 1;
+            while(arr[j] > pivot and j >= low):
+                j -= 1;
+            if i < j:
+                arr[i], arr[j] = arr[j], arr[i];
+        arr[low], arr[j] = arr[j], arr[low];
+        return j;
+    
