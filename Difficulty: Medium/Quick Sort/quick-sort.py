265fabd438e1c0e50b7d0fa0bcb3abeb83964001
Anusha Syeda <anusha.bcss19@iba-suk.edu.pk>
2025-07-09 11:58:37 +0500

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Quick Sort/quick-sort.py
+++ Difficulty: Medium/Quick Sort/quick-sort.py
@@ -11,16 +11,16 @@ class Solution:
     
     def partition(self,arr,low,high):
         # code here
-        pivot = arr[low];
+        pivot = arr[high];
         i = low;
         j = high;
         while i < j:
-            while(arr[i] <= pivot and i <= high-1):
+            while(arr[i] < pivot and i <= high-1):
                 i += 1;
-            while(arr[j] > pivot and j >= low):
+            while(arr[j] >= pivot and j >= low):
                 j -= 1;
             if i < j:
                 arr[i], arr[j] = arr[j], arr[i];
-        arr[low], arr[j] = arr[j], arr[low];
-        return j;
+        arr[high], arr[i] = arr[i], arr[high];
+        return i;
     
