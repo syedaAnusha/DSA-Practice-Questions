0881873af5237858da3f988008fa153bdc453d3a
Anusha Syeda <anusha.bcss19@iba-suk.edu.pk>
2025-07-28 22:40:32 +0500

    18 ms (28.99%) 12.6 MB (16.78%) 07-28-2025 0052-n-queens-ii Hard .py

---
+++ LeetCode/Hard/0052-n-queens-ii/0052-n-queens-ii-07-28-2025-22-40-30.py
@@ -0,0 +1,38 @@
+class Solution(object):
+    def findNQueens(self, n, col, leftRow, upperDiagonal, lowerDiagonal, board):
+        # Approach 2
+        if col == n:
+            return 1;
+        else:
+            totalQueens = 0;
+            for row in range(0, n):
+                if leftRow[row] == 0 and upperDiagonal[n-1 + col-row] == 0 and lowerDiagonal[row+col] == 0:
+                    curr_row = list(board[row]);
+                    curr_row[col] = 'Q';
+                    board[row] = "".join(curr_row);
+                    leftRow[row] = 1;
+                    upperDiagonal[n-1 + col-row] = 1;
+                    lowerDiagonal[row+col] = 1;
+                    totalQueens += self.findNQueens(n, col+1, leftRow, upperDiagonal, lowerDiagonal,board);
+                    curr_row = list(board[row]);
+                    curr_row[col] = '.';
+                    board[row] = "".join(curr_row);
+                    leftRow[row] = 0;
+                    upperDiagonal[n-1 + col-row] = 0;
+                    lowerDiagonal[row+col] = 0;
+        return totalQueens;
+                
+
+
+    def totalNQueens(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        board = [n*'.' for _ in range(n)];
+        leftRow = [0 for _ in range(n)];
+        upperDiagonal = [0 for _ in range(2*n)];
+        lowerDiagonal = [0 for _ in range(2*n)];
+
+        return self.findNQueens(n, 0, leftRow, upperDiagonal, lowerDiagonal,board);
+        
