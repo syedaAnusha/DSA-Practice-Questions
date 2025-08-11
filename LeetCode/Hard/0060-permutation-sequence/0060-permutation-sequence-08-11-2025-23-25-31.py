be00cc2fe98240e79e456c5999052e4c1d016913
Anusha Syeda <anusha.bcss19@iba-suk.edu.pk>
2025-08-11 23:25:32 +0500

    0 ms (100%) 18 MB (37.64%) 08-11-2025 0060-permutation-sequence Hard .py

---
+++ LeetCode/Hard/0060-permutation-sequence/0060-permutation-sequence-08-11-2025-23-25-31.py
@@ -0,0 +1,33 @@
+class Solution:
+    # Time Complexity : O(n)*O(n)
+    # Space Complexity: O(n)
+    def findPermutations(self, n, k):
+        fact = 1;
+        numbers = [];
+        for i in range(1, n):
+            fact = fact * i;
+            numbers.append(i); 
+        numbers.append(i+1);
+        ans = '';
+        k = k-1;
+        while True:
+            index = k // fact;
+            ans = ans + str(numbers[index]);
+            numbers.remove(numbers[index]);
+            if len(numbers) == 0:
+                break;
+
+            k = k % fact;
+            fact = fact // len(numbers);
+        return ans;
+
+    def getPermutation(self, n: int, k: int) -> str:
+        if n == 1 and k == 1:
+            return '1';
+        else:
+            return self.findPermutations(n, k);
+        
+            
+        
+
+        
