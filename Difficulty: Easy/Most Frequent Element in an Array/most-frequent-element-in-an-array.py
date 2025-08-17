ea91cafd513e41a842de7f707502e8c19b67bf93
Anusha Syeda <anusha.bcss19@iba-suk.edu.pk>
2025-08-17 14:19:17 +0500

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Most Frequent Element in an Array/most-frequent-element-in-an-array.py
@@ -0,0 +1,22 @@
+class Solution:
+    def mostFreqEle(self, arr):
+        # code here
+        hashTable = {};
+        maxFreq = 0;
+        highestElem = float('-inf');
+        
+        
+        for i in range(len(arr)):
+            if arr[i] in hashTable:
+                hashTable[arr[i]] += 1;
+            else:
+                hashTable[arr[i]] = 1;
+            maxFreq = max(maxFreq, hashTable[arr[i]]);
+            
+        for key, value in hashTable.items():
+            if value == maxFreq:
+                highestElem = max(highestElem, key);
+                
+        return highestElem;
+                
+        
