0ab3e6bee5d47fbe2864ceb756493eea6e51f05e
Anusha Syeda <anusha.bcss19@iba-suk.edu.pk>
2025-08-20 11:21:05 +0500

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Bubble Sort/bubble-sort.py
+++ Difficulty: Easy/Bubble Sort/bubble-sort.py
@@ -1,18 +1,23 @@
 class Solution:
+    # T.C = O(n^2) - Worst Case & Avg.Case O(n): Best Case
+    # S.C = O(1)
     def swap(self, arr, i, j):
         arr[i], arr[j] = arr[j], arr[i];
+    
+    def bubbleSortArr(self, arr, i, didSwap):
+        if i < 0:
+            return;
+        for j in range(0, i):
+            if arr[j] > arr[j+1]:
+                self.swap(arr, j, j+1);
+                didSwap = True;
+                
+        if not didSwap:
+            return;
+        self.bubbleSortArr(arr, i-1, didSwap);
         
     def bubbleSort(self,arr):
         # code here
-        i = len(arr)-1;
-        j = 0;
-        while i >= 0:
-            didSwap = 0;
-            for j in range(0, i):       
-                if arr[j] > arr[j+1]:
-                    self.swap(arr,j, j+1);
-                    didSwap = 1;
-            if didSwap == 0:
-                break;
-            i -= 1;
+        self.bubbleSortArr(arr, len(arr)-1, False);
         return arr;
+        
