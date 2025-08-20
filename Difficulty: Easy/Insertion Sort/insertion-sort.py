f7d8d2de021416c4e4ac590f4437203caff9d7c4
Anusha Syeda <anusha.bcss19@iba-suk.edu.pk>
2025-08-20 10:51:58 +0500

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Insertion Sort/insertion-sort.py
@@ -0,0 +1,17 @@
+class Solution:
+    # Time Complexity:
+    # Worst & Avg. Case -> O(n^2)
+    # Best Case -> O(n)
+    # Space Complexity: O(1)
+    
+    def swap(self, arr, i, j):
+        arr[i], arr[j] = arr[j], arr[i];
+        
+    def insertionSort(self, arr):
+        # code here
+        for i in range(len(arr)):
+            j = i;
+            while j > 0 and arr[j] < arr[j-1]:
+                self.swap(arr, j, j-1);
+                j -= 1;
+        return arr;
