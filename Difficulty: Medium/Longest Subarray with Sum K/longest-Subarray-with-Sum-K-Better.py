d820b2aa759dcd6372b3194486c6a81a57cfc5ce
Anusha Syeda <anoshasyeda1999@gmail.com>
2025-08-28 23:58:09 +0500

    Add solution for longest subarray with sum K
    Implemented a solution to find the longest subarray with a given sum using a hash set for efficient lookups.

---
+++ Difficulty: Medium/Longest Subarray with Sum K/longest-Subarray-with-Sum-K-Better.py
@@ -0,0 +1,22 @@
+class Solution:
+# Better Approach, for arrays contain [0, Positives]
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+def longestSubarray(self, arr, k):
+    # code here
+    maxLen = 0;
+    Sum = 0;
+    hashSet = {};
+    for i in range(len(arr)):
+        Sum += arr[i];
+
+        if Sum not in hashSet:
+            hashSet[Sum] = i;
+
+        if Sum == k:
+            maxLen = max(maxLen, i+1);
+
+        rem = Sum - k;
+        if rem in hashSet:
+            maxLen = max(maxLen, i-hashSet[rem]);
+    return maxLen;
