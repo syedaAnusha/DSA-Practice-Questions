5cde0b3edb09503c1e0e9192dcb81fe7aa16f8db
Anusha Syeda <anoshasyeda1999@gmail.com>
2025-08-28 23:57:01 +0500

    Optimize longest subarray with sum K solution
    Refactor longest subarray function to use two pointers and improve efficiency.

--- Difficulty: Medium/Longest Subarray with Sum K/Longest-Subarray-with-Sum-K-Optimal.py
+++ Difficulty: Medium/Longest Subarray with Sum K/Longest-Subarray-with-Sum-K-Optimal.py
@@ -1,24 +1,24 @@
-class Solution:
-    # Optimal Approach for array contain only positive numbers
-    # Time Complexity: O(n)
-    # Space Complexity: O(n)
-    
-    def longestSubarray(self, arr, k):  
-        # code here
-        maxLen = 0;
+class Solution(object):
+		# Optimal Solution - Using Two pointers for arrays contain only [0, positives]
+		# Time Complexity: O(2n) = O(n)
+		# Space Complexity: O(1)
+    def subarraySum(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        left = 0;
+        right = 0;
         Sum = 0;
-        hashSet = {};
-        for i in range(len(arr)):
-            Sum += arr[i];
-            hashSet[Sum] = i;
-            
+        maxLen = 0;
+        while left <= right and right < len(nums):
+            Sum += nums[right];
             if Sum == k:
-                maxLen = max(maxLen, i+1);
-            
-            rem = Sum - k;
-            if rem in hashSet:
-                maxLen = max(maxLen, i-hashSet[rem]);
+                maxLen = max(maxLen, right-left+1);
+            if Sum > k:
+                Sum -= nums[left];
+                left += 1;
+            if Sum <= k:
+                right += 1;
         return maxLen;
-        
-            
-    
