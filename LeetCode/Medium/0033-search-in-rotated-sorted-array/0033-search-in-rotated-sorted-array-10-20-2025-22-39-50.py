9f5e5b54aac2403698bbb393c308ffc1b112e509
Anusha Syeda <anusha.bcss19@iba-suk.edu.pk>
2025-10-20 22:39:51 +0500

    0 ms (100%) 18.1 MB (66.56%) 10-20-2025 0033-search-in-rotated-sorted-array Medium .py

---
+++ LeetCode/Medium/0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array-10-20-2025-22-39-50.py
@@ -0,0 +1,29 @@
+class Solution:
+    # Optimal Approach
+    # Time Complexity: O(log n)
+    # Space Complexity: O(1)
+    def search(self, nums: List[int], target: int) -> int:
+        n = len(nums);
+        low = 0;
+        high = n - 1;
+
+        while low <= high:
+            mid = (low + high) // 2;
+            if nums[mid] == target:
+                return mid;
+
+            # if left is sorted
+            elif nums[low] <= nums[mid]:
+                if nums[low] <= target and target <= nums[mid]:
+                    high = mid - 1;
+                else:
+                    low = mid + 1;
+
+            # else right is sorted
+            else:
+                if nums[mid] <= target and target <= nums[high]:
+                    low = mid + 1;
+                else:
+                    high = mid - 1;
+        return -1;
+        
