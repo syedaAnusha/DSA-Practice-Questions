ce8d5bbcd47331285f4cd68a0f48afce1863bec0
Anusha Syeda <anusha.bcss19@iba-suk.edu.pk>
2025-10-24 22:49:38 +0500

    0 ms (100%) 18 MB (18.2%) 10-24-2025 0162-find-peak-element Medium .py

---
+++ LeetCode/Medium/0162-find-peak-element/0162-find-peak-element-10-24-2025-22-49-37.py
@@ -0,0 +1,31 @@
+class Solution:
+    # Optimal Approach
+    # Time Complexity: O(log n)
+    # Space Complexity: O(1)
+    def findPeakElement(self, nums: List[int]) -> int:
+        n = len(nums);
+        if n == 1:
+            return 0;
+        if nums[0] > nums[1]:
+            return 0;
+        if nums[n-1] > nums[n-2]:
+            return n-1;
+        
+        low = 1;
+        high = n-2;
+        while low <= high:
+            mid = (low + high) // 2;
+            if nums[mid-1] < nums[mid] and nums[mid] > nums[mid+1]:
+                return mid;
+            elif nums[mid-1] < nums[mid]:
+                low = mid+1;
+            else:
+                high = mid - 1;
+        return -1;
+
+
+
+
+           
+
+        
