3b39df8b0632b98fface6f3d53df46c3dfd36077
Anusha Syeda <anusha.bcss19@iba-suk.edu.pk>
2025-11-08 17:45:46 +0500

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Minimize Max Distance to Gas Station/minimize-max-distance-to-gas-station.py
+++ Difficulty: Hard/Minimize Max Distance to Gas Station/minimize-max-distance-to-gas-station.py
@@ -1,26 +1,36 @@
-import heapq
 class Solution:
-    # Brute Force Approach
-    # Time Complexity: O(n * log n) + O(k * log n);
-    # Space Complexity: O(n-1), where n = length of station
+    # Optimal Approach
+    # Time Complexity: O(n) + O(n * log (max_distance));
+    # Space Complexity: O(1)
+    def countNumOfGasStations(self, stations, distance):
+        cnt = 0;
+        n = len(stations);
+        for i in range(1, n):
+            gap = stations[i] - stations[i-1]
+            cnt += int(gap / distance)  # Use normal division, not floor division
+            
+            #possibleGasStations = int((stations[i] - stations[i-1]) / distance);
+            #if ((stations[i] - stations[i-1]) / distance) == (possibleGasStations * distance):
+            #    possibleGasStations -= 1;
+            #else:
+            #    cnt += possibleGasStations;
+        return cnt;
+        
     def minMaxDist(self, stations, k):
         # Code here
         n = len(stations);
-        howMany = [0 for _ in range(n)];
-        pq = [];
+        low = 0;
+        high = 0;
+        for i in range(1, n):
+            distance = stations[i] - stations[i-1];
+            high = max(high, distance);
         
-        if len(stations) <= 1:
-            return 0.00;
-            
-        for i in range(n-1):
-            diff = (stations[i+1] - stations[i]);
-            heapq.heappush(pq, (-diff, i))
-        
-        for gasStation in range(1,k+1):
-            Distance, index = heapq.heappop(pq);
-            howMany[index] += 1;
-            original_gap = stations[index+1] - stations[index]
-            sectionLength = original_gap / (howMany[index] + 1);
-            heapq.heappush(pq, (-sectionLength, index))
-            
-        return -pq[0][0]; 
+        diff = 1e-6;
+        while high - low  > diff:
+            mid = (low + high) / 2;
+            cnt = self.countNumOfGasStations(stations, mid);
+            if cnt > k:
+                low = mid;
+            else:
+                high = mid;
+        return high;
