afbaa64abd14049f3f39668eb5dd1ca9e89237a1
Anusha Syeda <anusha.bcss19@iba-suk.edu.pk>
2026-01-05 23:04:14 +0500

    60 ms (12.25%) 39.1 MB (37.83%) 01-05-2026 0234-palindrome-linked-list Easy .py

---
+++ LeetCode/Easy/0234-palindrome-linked-list/0234-palindrome-linked-list-01-05-2026-23-04-13.py
@@ -0,0 +1,51 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    # Optimal Approach
+    # Time Complexity: O(n/2) + O(n/2) = O(2n)
+    # Space Complexity: O(1)
+    def findMidNode(self, temp):
+        slowPtr = temp;
+        fastPtr = temp;
+
+        while fastPtr.next != None and fastPtr.next.next != None:
+            slowPtr = slowPtr.next;
+            fastPtr = fastPtr.next.next;
+        return slowPtr;
+    
+    def reverseLinkedList(self, node):
+        if node == None or node.next == None:
+            return node;
+        prevNode = None;
+        while node != None:
+            nextNode = node.next;
+            node.next = prevNode;
+            prevNode = node;
+            node = nextNode
+        return prevNode;
+      
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        if head == None or head.next == None:
+            return True;
+        
+        temp = head;
+      
+        # find middle node
+        midNode = self.findMidNode(temp);
+        newHead = self.reverseLinkedList(midNode.next);
+
+        first = head;
+        last = newHead;
+
+        while last != None:
+            if first.val != last.val:
+                self.reverseLinkedList(newHead);
+                return False
+            first = first.next;
+            last = last.next;
+        self.reverseLinkedList(newHead);
+        return True;
+
