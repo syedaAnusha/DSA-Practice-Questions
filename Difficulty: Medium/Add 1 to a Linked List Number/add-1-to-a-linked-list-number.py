628913c7fa6e0a4b1e9feafa069903727b3d161c
Anusha Syeda <anusha.bcss19@iba-suk.edu.pk>
2026-01-19 22:45:16 +0500

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Add 1 to a Linked List Number/add-1-to-a-linked-list-number.py
+++ Difficulty: Medium/Add 1 to a Linked List Number/add-1-to-a-linked-list-number.py
@@ -7,48 +7,28 @@ class Node:
 '''
 
 class Solution:
-    # Brute Force Approach
-    # Time Complexity: O(n) + O(n) = O(2n)
-    # Space Complexity: O(n)
-    def reverseList(self, head):
-        prevNode = None;
-        temp = head;
-        while temp != None:
-            frontNode = temp.next;
-            temp.next = prevNode;
-            prevNode = temp;
-            temp = frontNode;
-        return prevNode;
+    # Optimal Approach
+    # Time Complexity: O(n) 
+    # Space Complexity: O(1)
+    # Aux Space for stack recursion: O(n)
+    def addOneToList(self, head):
+        if head == None:
+            return 1;
+        
+        carry = self.addOneToList(head.next);
+        Sum = head.data + carry;
+        head.data = Sum % 10; 
+        carry = Sum // 10;
+        return carry;
         
     def addOne(self,head):
         if head == None:
-            return head;
-        #Returns new head of linked List.
-        newHead = self.reverseList(head);
-        temp = newHead;
-        dummyNode = None;
-        nodeOne = Node(1);
-        carry = 0;
-        mover = dummyNode;
+            return None;
+       
+        carry = self.addOneToList(head);
+        if carry == 1:
+            newHead = Node(carry);
+            newHead.next = head;
+            return newHead;
+        return head;
         
-        while temp != None or nodeOne != None:
-            Sum = carry;
-            if temp != None:
-                Sum += temp.data;
-            if nodeOne != None:
-                Sum += nodeOne.data;
-            newNode = Node(Sum % 10);
-            newNode.next = mover;
-            mover = newNode;
-            carry = Sum // 10;
-            
-            if temp:
-                temp = temp.next;
-            if nodeOne:
-                nodeOne = nodeOne.next;
-        if carry:
-            newNode = Node(carry);
-            newNode.next = mover;
-            mover = newNode;
-            
-        return mover;
