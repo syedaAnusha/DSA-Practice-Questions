a7bbc84ea7f046266f85afb93093aa0a680edd6c
Anusha Syeda <anusha.bcss19@iba-suk.edu.pk>
2026-02-01 14:17:29 +0500

    0 ms (100%) 19.3 MB (43.79%) 02-01-2026 1922-count-good-numbers Medium .py

---
+++ LeetCode/Medium/1922-count-good-numbers/1922-count-good-numbers-02-01-2026-14-17-26.py
@@ -0,0 +1,22 @@
+class Solution:
+    # Optimal Approach - Iterative Approach
+    # Time Complexity: O(2log n)
+    # Space Complexity: O(1)
+    MOD = 1_000_000_007;
+    def myPow(self, x, n):
+        ans = 1;
+        while n > 0:
+            if n & 1:
+                ans = (ans * x) % self.MOD;
+                n -= 1;
+            else:
+                x = (x * x) % self.MOD;
+                n >>= 1;
+        return ans;
+
+    def countGoodNumbers(self, n: int) -> int:
+        countOfEvens = (n + 1)>>1;
+        countOfPrimes = n>>1;    
+        evenNums = self.myPow(5, countOfEvens);
+        primeNums = self.myPow(4, countOfPrimes);
+        return (evenNums * primeNums) % self.MOD; 
