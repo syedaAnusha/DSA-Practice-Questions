4768eed7615052511c30b8d56098803cd85d1a2d
Anusha Syeda <anoshasyeda1999@gmail.com>
2026-02-03 00:07:27 +0500

    Add brute force recursive stack sorting solution
    Implement a brute force recursive solution to sort a stack.

---
+++ Difficulty: Medium/Sort a stack/sort-a-stack-brute-using-recursion.py
@@ -0,0 +1,19 @@
+class Solution:
+    # Brute Approach - 01
+    # Time Complexity:O(n^2)
+    # Space Complexity:O(n)
+    def insertVal(self, st, curVal):
+        if not st or curVal > st[-1]:
+            st.append(curVal);
+            return st;
+        val = st.pop();
+        self.insertVal(st, curVal);
+        st.append(val);
+        return st;
+        
+    def sortStack(self, st):
+        # code here 
+        if st:
+            curVal = st.pop();
+            self.sortStack(st);
+            self.insertVal(st, curVal);
