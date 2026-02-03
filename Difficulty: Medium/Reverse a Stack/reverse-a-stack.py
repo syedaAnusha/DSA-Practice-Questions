6b49c6aeb819648e19bffc1e4c64eb333fa42c44
Anusha Syeda <anusha.bcss19@iba-suk.edu.pk>
2026-02-03 23:16:25 +0500

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Reverse a Stack/reverse-a-stack.py
@@ -0,0 +1,20 @@
+class Solution:
+    # Brute Approach
+    # Time Complexity:O(n^2)
+    # Space Complexity: O(n^2)
+    def helperReverseStack(self, stack, curVal):
+        if not stack:
+            stack.append(curVal);
+            return stack;
+            
+        val = stack.pop();
+        self.helperReverseStack(stack, curVal);
+        stack.append(val);
+        
+        
+    def reverseStack(self, st):
+        # code here
+        if st:
+            curVal = st.pop();
+            self.reverseStack(st);
+            self.helperReverseStack(st, curVal);
