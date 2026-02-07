b76dcb5992ac77fa0cd1b9167567ff06d00f6a6e
Anusha Syeda <anoshasyeda1999@gmail.com>
2026-02-07 12:58:22 +0500

    Refine complexity comments in subset-sums.py
    Updated time and space complexity comments for clarity.

--- Difficulty: Medium/Subset Sums/subset-sums.py
+++ Difficulty: Medium/Subset Sums/subset-sums.py
@@ -1,14 +1,12 @@
 class Solution:
     # Brute Force Approach
-    # Time Complexity: O(2^n)*O(n)
-    # Space Complexity: O(2^n) + O(2^n) for recursive calls, where k is the length of sumsArr
+    # Time Complexity: O(2^n)
+    # Space Complexity: O(2^n) + O(n) for recursive calls
     def generateSubsetsSums(self,arr, sumsArr, index, Sum):
-        #sumsArr.append(Sum);
         for i in range(index, len(arr)):
             curSum = Sum + arr[i];
             sumsArr.append(curSum);
             self.generateSubsetsSums(arr, sumsArr, i+1, curSum);
-            #sumsArr.remove(curSum);
             
 	def subsetSums(self, arr):
 		# code here
