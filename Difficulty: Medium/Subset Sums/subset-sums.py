ccc984d485ec01c888bd826955fc9495bcc05931
Anusha Syeda <anusha.bcss19@iba-suk.edu.pk>
2026-02-07 12:39:33 +0500

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Subset Sums/subset-sums.py
+++ Difficulty: Medium/Subset Sums/subset-sums.py
@@ -1,18 +1,19 @@
-#User function Template for python3
 class Solution:
-    def findSubsetSums(self, arr, tempArr, ans, index):
-        if index == len(arr):
-            Sum = sum(tempArr)
-            ans.append(Sum);
-            return;
-        else:
-            tempArr.append(arr[index]);
-            self.findSubsetSums(arr, tempArr, ans, index+1);
-            tempArr.remove(arr[index]);
-            self.findSubsetSums(arr, tempArr, ans, index+1);
+    # Brute Force Approach
+    # Time Complexity: O(2^n)*O(n)
+    # Space Complexity: O(2^n) + O(2^n) for recursive calls, where k is the length of sumsArr
+    def generateSubsetsSums(self,arr, sumsArr, index, Sum):
+        #sumsArr.append(Sum);
+        for i in range(index, len(arr)):
+            curSum = Sum + arr[i];
+            sumsArr.append(curSum);
+            self.generateSubsetsSums(arr, sumsArr, i+1, curSum);
+            #sumsArr.remove(curSum);
             
 	def subsetSums(self, arr):
 		# code here
-		ans = [];
-		self.findSubsetSums(arr, [], ans, 0);
-		return ans;
+		sumsArr = [0];
+		index = 0;
+		Sum = 0;
+		self.generateSubsetsSums(arr, sumsArr, index, Sum);
+		return sumsArr;
