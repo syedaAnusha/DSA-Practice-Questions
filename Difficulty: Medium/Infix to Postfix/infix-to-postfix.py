54f21d422cd4faee20182aa150a44d354ac736fc
Anusha Syeda <anusha.bcss19@iba-suk.edu.pk>
2026-02-28 20:57:04 +0500

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Infix to Postfix/infix-to-postfix.py
@@ -0,0 +1,47 @@
+class Solution:
+    # Time Complexity: O(n) + O(n) , where O(n) for outer and O(n) for inner loop
+    # Space Complexity: O(n) + O(n) where O(n) for stack and O(n) for postfixExp variable
+    def getPriorityOfAn(self, operator):
+        if operator == '^':
+            return 3;
+        elif operator == '*' or operator == '/':
+            return 2;
+        elif operator == '+' or operator == '-':
+            return 1;
+        else:
+            return -1;
+            
+    def isNotRightAssociative(self, operator):
+        if operator == '^':
+            return False;
+        return True;
+            
+    def infixtoPostfix(self, s):
+        #code here
+        postfixExp = "";
+        stack = [];
+     
+        for i in range(len(s)):
+            if s[i].isalnum():
+                postfixExp += s[i];
+                
+            elif s[i] == '(':
+                stack.append(s[i]);
+                
+            elif s[i] == ')':
+                while stack and stack[-1] != '(':
+                    postfixExp += stack[-1];
+                    stack.pop();
+                stack.pop();
+                
+            else:
+                while stack and self.getPriorityOfAn(s[i]) <= self.getPriorityOfAn(stack[-1]) and self.isNotRightAssociative(s[i]):
+                    postfixExp += stack[-1];
+                    stack.pop();
+                stack.append(s[i]);
+            
+        while stack:
+            postfixExp += stack[-1];
+            stack.pop();
+        
+        return postfixExp;
