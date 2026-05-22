611153bbef21503ff9886ff49532f9821ac57db5
Anusha Syeda <anusha.bcss19@iba-suk.edu.pk>
2026-05-22 17:43:23 +0500

    66 ms (30.43%) 22.1 MB (50.24%) 05-22-2026 1004-max-consecutive-ones-iii Medium .py

---
+++ LeetCode/Medium/1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii-05-22-2026-17-43-22.py
@@ -0,0 +1,20 @@
+class Solution:
+    # Optimal Approach - 01
+    # Time Complexity: O(2n)
+    # Space Complexity: O(1)
+    def longestOnes(self, nums: List[int], k: int) -> int:
+        maxLen = 0;
+        lenOfNums = len(nums);
+        left = right = 0;
+        zeroes = 0;
+        while right < lenOfNums:
+            if nums[right] == 0:
+                zeroes += 1;
+            while zeroes > k:
+                if nums[left] == 0:
+                    zeroes -= 1;
+                left += 1;
+            maxLen = max(maxLen, right-left+1);
+            right += 1;
+
+        return maxLen;  
