63f512d710796f21ede3f905a3d303f2d51d642b
Anusha Syeda <anusha.bcss19@iba-suk.edu.pk>
2026-05-23 01:33:21 +0500

    203 ms (24.82%) 25.9 MB (63.1%) 05-23-2026 0904-fruit-into-baskets Medium .py

---
+++ LeetCode/Medium/0904-fruit-into-baskets/0904-fruit-into-baskets-05-23-2026-01-33-20.py
@@ -0,0 +1,23 @@
+class Solution:
+    # Optimal Approach - 01
+    # Time Complexity: O(2n)
+    # Space Complexity: O(3) // atmost 3 unique elements 
+    def totalFruit(self, fruits: List[int]) -> int:
+        maxFruits = 0;
+        totalFruits = len(fruits);
+        left = right = 0;
+        setBasket = {};
+        while right < totalFruits:
+            if fruits[right] in setBasket:
+                setBasket[fruits[right]] += 1;
+            else:
+                setBasket[fruits[right]] = 1;
+            if len(setBasket) <= 2:
+                maxFruits = max(maxFruits,right-left+1);
+            while len(setBasket) > 2:
+                setBasket[fruits[left]] -= 1;
+                if setBasket[fruits[left]] == 0:
+                    setBasket.pop(fruits[left]);
+                left += 1;
+            right += 1;
+        return maxFruits;
