a7b30cc913c75b30d82deb57b88cfd5a56ea50c2
Anusha Syeda <anoshasyeda1999@gmail.com>
2026-06-02 16:59:30 +0500

    Refactor heapify method in heap-sort.py
    Refactor heapify method to use a while loop for clarity and efficiency.

--- Difficulty: Medium/Heap Sort/heap-sort.py
+++ Difficulty: Medium/Heap Sort/heap-sort.py
@@ -1,36 +1,26 @@
 class Solution:
-    # Optimal Approach
-    # Time Complexity: O(nlogn) + O(n)
-    # Space Complexity: O(log n)
+    # Optimal Approach - 02
+    # Time Complexity: O(nlogn)
+    # Space Complexity: O(1)
     def swap(self, arr, i, j):
         arr[i], arr[j] = arr[j], arr[i];
     
-    def findLargestNodeIndex(self, arr, index, size):
-        largest = index;
-        left = 2*index+1;
-        right = 2*index+2;
-        
-        if left < size and arr[left] > arr[largest]:
-            largest = left;
-        if right < size and arr[right] > arr[largest]:
-            largest = right;
-        
-        return largest;
-        
-        
     def heapify(self, arr, index, size):
-        largest = index;
-        left = 2*index+1;
-        right = 2*index+2;
+        while True:
+            largest = index;
+            left = 2*index+1;
+            right = 2*index+2;
         
-        if left < size and arr[left] > arr[largest]:
-            largest = left;
-        if right < size and arr[right] > arr[largest]:
-            largest = right;
+            if left < size and arr[left] > arr[largest]:
+                largest = left;
+            if right < size and arr[right] > arr[largest]:
+                largest = right;
         
-        if largest != index:
-            self.swap(arr, index, largest);
-            self.heapify(arr, largest, size);
+            if largest != index:
+                self.swap(arr, index, largest);
+                index = largest;
+            else:
+                break;
             
     def convertToMaxHeap(self, arr):
         N = len(arr);
