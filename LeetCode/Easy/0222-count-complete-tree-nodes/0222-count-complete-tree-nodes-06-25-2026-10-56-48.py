bf873d1acc7aa1321f5c6924d1ea0dbbd8dc0091
Anusha Syeda <anusha.bcss19@iba-suk.edu.pk>
2026-06-25 10:56:50 +0500

    3 ms (48.01%) 24 MB (18.12%) 06-25-2026 0222-count-complete-tree-nodes Easy .py

---
+++ LeetCode/Easy/0222-count-complete-tree-nodes/0222-count-complete-tree-nodes-06-25-2026-10-56-48.py
@@ -0,0 +1,30 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    # Optimal Approach
+    # Time Complexity: O(n)
+    # Space Complexity: O(n)
+    def findNumOfNodes(self, root, cntNodes):
+        if not root or (not root.left and not root.right):
+            return
+
+        if root.left:
+            cntNodes[0] += 1 
+        self.findNumOfNodes(root.left,  cntNodes)
+        if root.right:
+            cntNodes[0] += 1 
+        self.findNumOfNodes(root.right, cntNodes)
+
+    def countNodes(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        if not root.left and not root.right:
+            return 1
+
+        cntNodes = [1]
+        self.findNumOfNodes(root, cntNodes)
+        return cntNodes[0]   
