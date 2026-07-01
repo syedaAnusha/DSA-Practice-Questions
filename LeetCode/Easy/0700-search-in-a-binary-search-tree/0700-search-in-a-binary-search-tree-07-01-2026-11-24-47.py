478344ec484df361e4d8b20e2cd9b0d7933c7363
Anusha Syeda <anusha.bcss19@iba-suk.edu.pk>
2026-07-01 11:24:49 +0500

    0 ms (100%) 21 MB (37.76%) 07-01-2026 0700-search-in-a-binary-search-tree Easy .py

---
+++ LeetCode/Easy/0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree-07-01-2026-11-24-47.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    # Optimal Approach - 02
+    # Time Complexity: O(log N)
+    # Space Complexity:O(1)
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        while root and root.val != val:
+            if val < root.val:
+                root = root.left
+            elif val > root.val:
+                root = root.right
+        return root
