5e065ef3a2fb4b21498242cabf9d575c869043f9
Anusha Syeda <anusha.bcss19@iba-suk.edu.pk>
2026-07-27 13:23:01 +0500

    3 ms (75.08%) 20.3 MB (89.9%) 07-27-2026 0210-course-schedule-ii Medium .py

---
+++ LeetCode/Medium/0210-course-schedule-ii/0210-course-schedule-ii-07-27-2026-13-22-59.py
@@ -0,0 +1,36 @@
+class Solution:
+    # Optimal Approach
+    # Time Complexity: O(n + E)
+    # Space Complexity: O(n) + O(n)
+    def findAdjList(self, V, edges, indegree):
+        adj = [[] for _ in range(V)]
+        for a, b in edges:
+            adj[b].append(a)
+            indegree[a] += 1
+
+        return adj
+    
+    def bfs(self, q, adj, indegree):
+        topo = []
+        while q:
+            node = q.popleft()
+            topo.append(node)
+            for adjNode in adj[node]:
+                indegree[adjNode] -= 1
+                if indegree[adjNode] == 0:
+                    q.append(adjNode)
+
+        return topo
+
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        indegree = [0 for _ in range(numCourses)]
+        adj = self.findAdjList(numCourses, prerequisites, indegree)
+        que = deque([])
+        for i in range(len(indegree)):
+            if indegree[i] == 0:
+                que.append(i)
+
+        topo = self.bfs(que, adj, indegree)
+        if len(topo) < numCourses:
+            return []
+        return topo   
