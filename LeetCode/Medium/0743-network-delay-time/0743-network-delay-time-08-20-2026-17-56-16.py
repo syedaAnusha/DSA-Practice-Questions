c79e2349c896b23101099bb3ee33fc45039d70b0
Anusha Syeda <anusha.bcss19@iba-suk.edu.pk>
2026-08-20 17:56:17 +0500

    323 ms (92.94%) 21.4 MB (39.25%) 08-20-2026 0743-network-delay-time Medium .py

---
+++ LeetCode/Medium/0743-network-delay-time/0743-network-delay-time-08-20-2026-17-56-16.py
@@ -0,0 +1,34 @@
+import heapq
+class Solution:
+    # Optimal Approach
+    # Time Complexity: O(e+v log v) + O(v)
+    # Space Complexity: O(v+e) 
+    def findAdjList(self, n, times):
+        adj = [[]*n for _ in range(n)]
+        for u, v, w in times:
+            adj[u].append((v, w))
+
+        return adj
+
+    def findMinTime(self, adj, pq, timesArr, times):
+        while pq:
+            time, source = heapq.heappop(pq)
+            for target, curTime in adj[source]:
+                if curTime + time < timesArr[target]:
+                    heapq.heappush(pq, (curTime + time, target))
+                    timesArr[target] = curTime + time
+        
+
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        adj = self.findAdjList(n+1, times)
+        pq = []
+        timesArr = [float('inf')] * (n+1)
+        timesArr[k] = 0
+        heapq.heappush(pq, (0, k))
+        self.findMinTime(adj, pq, timesArr, times)
+        minTime = 0
+        for i in range(1,len(timesArr)):
+            if timesArr[i] == float('inf'):
+                return -1
+            minTime = max(minTime, timesArr[i])
+        return minTime 
